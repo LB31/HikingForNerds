dc7173c59bbf0bf6070e4253e4d357fe589bb449
Jakub <jakub94@gmx.de>
2019-11-07 15:38:58 +0100

    map as component

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:flutter_map/plugin_api.dart';
-import 'package:latlong/latlong.dart';
-import '../services/types.dart';
-import 'package:hiking4nerds/services/map_service.dart';
+import 'package:hiking4nerds/components/map.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -12,44 +8,16 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  MapController mc = MapController();
-  MapService ms = MapService();
 
   @override
   Widget build(BuildContext context) {
-    LatLng mapLocation = ms.getMapLatLong();
-    TileLayerOptions tileLayerOptions = ms.getTileLayerOptions(tl: TileLayerType.hike);
-    PolylineLayerOptions polylineLayerOptions = ms.getPolyLineLayerOptions();
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Hiking 4 Nerds'),
         backgroundColor: Color(0xff76B900),
       ),
-      body: FlutterMap(
-        mapController: this.mc,
-        options: new MapOptions(center: mapLocation),
-        layers: [
-          tileLayerOptions,
-          polylineLayerOptions,
-          new MarkerLayerOptions(markers: [
-            new Marker(
-                width: 45.0,
-                height: 45.0,
-                point: mapLocation,
-                builder: (context) => Container(
-                  child: IconButton(
-                      icon: Icon(
-                          Icons.accessibility,
-                        color: Colors.redAccent,
-                      ),
-                      onPressed: () {
-                        print('Marker tapped!');
-                      }),
-                ))
-          ]),
-        ],
-      ),
+      body: new Map()
     );
   }
 }
