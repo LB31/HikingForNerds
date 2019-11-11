668e8b33c0f3f70a73629ca1be6f5cb0940118a2
Jakub <jakub94@gmx.de>
2019-11-11 15:37:35 +0100

    Add additional colors to styles.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,16 @@ import 'package:hiking4nerds/pages/home.dart';
 import 'styles.dart';
 
 void main() => runApp(MaterialApp(
-  title: "Hiking4Nerds",
-  initialRoute: '/',
-  routes: {
-    '/': (context) => Home(),
-  },
-  theme: ThemeData(
-      primaryColor: htwGreen,
-      accentColor: Colors.black,
-    iconTheme: IconThemeData(
-      color: htwGreen
-    ),
-    buttonTheme: ButtonThemeData(
-      buttonColor: htwGreen,
-    )
-  ),
-));
+      title: "Hiking4Nerds",
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Home(),
+      },
+      theme: ThemeData(
+          primaryColor: htwGreen,
+          accentColor: htwGrey,
+          iconTheme: IconThemeData(color: htwGreen),
+          buttonTheme: ButtonThemeData(
+            buttonColor: htwGreen,
+          )),
+    ));
--- lib/styles.dart
+++ lib/styles.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/material.dart';
 
+//Colors taken from: https://corporatedesign.htw-berlin.de/schrift-farbe/markenfarben/
 final Color htwGreen = Color(0xff76B900);
+final Color htwBlue = Color(0xff0082D1);
+final Color htwOrange = Color(0xffFF5F00);
+final Color htwGrey = Color(0xffAFAFAF);
+
+
+
+
+
