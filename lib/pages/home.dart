7ca05302042a29238e79865306e4ed78bd22df25
Mario Schuetz <mayo2@gmx.de>
2019-11-14 12:35:24 +0100

    Add page dummies

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -17,17 +17,34 @@ class _HomeState extends State<Home> {
         title: Text('Hiking 4 Nerds'),
         backgroundColor: Theme.of(context).primaryColor,
       ),
-      body: FabCircularMenu(
-        child: HikingMap(),
-        ringColor: Colors.white30,
-        fabColor: Theme.of(context).primaryColor,
-        options: <Widget>[
-          IconButton(icon: Icon(
-            Icons.help_outline), onPressed: () {}, iconSize: 48.0),
-          IconButton(icon: Icon(Icons.save_alt), onPressed: () {}, iconSize: 48.0),
-          IconButton(icon: Icon(Icons.map), onPressed: () {}, iconSize: 48.0),
-          IconButton(icon: Icon(Icons.info_outline), onPressed: () {}, iconSize: 48.0),
-        ],
+      body: Container(
+        child: FabCircularMenu(
+          child: HikingMap(),
+          ringColor: Colors.white30,
+          fabColor: Theme.of(context).primaryColor,
+          options: <Widget>[
+            IconButton(icon: Icon(
+              Icons.help_outline), 
+              onPressed: () { Navigator.pushNamed(context, '/help');},
+              iconSize: 48.0
+            ),
+            IconButton(icon: Icon(
+              Icons.info_outline), 
+              onPressed: () { Navigator.pushNamed(context, '/info');},
+              iconSize: 48.0
+            ),
+            IconButton(icon: Icon(
+              Icons.find_replace), 
+              onPressed: () { Navigator.pushNamed(context, '/routesetup');},
+              iconSize: 48.0
+            ),
+            IconButton(icon: Icon(
+              Icons.settings), 
+              onPressed: () { Navigator.pushNamed(context, '/settings');},
+              iconSize: 48.0
+            ),
+          ],
+        ),
       ),
     );
   }
