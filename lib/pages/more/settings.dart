9a67cacd5d7dccfb8842f409403b449f930403b0
Leonid <leonidb93@gmail.com>
2019-12-31 03:00:09 +0100

    add dummy checkbox

--- lib/pages/more/settings.dart
+++ lib/pages/more/settings.dart
@@ -7,13 +7,34 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  bool checkboxValue = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Settings'),
-        backgroundColor: Theme.of(context).primaryColor,
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Settings'),
+          backgroundColor: Theme.of(context).primaryColor,
+        ),
+        body: Container(
+          child: Stack(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(checkboxValue.toString()),
+                  Checkbox(
+                    value: checkboxValue,
+                    onChanged: (bool value) {
+                      setState(() {
+                        checkboxValue = value;
+                      });
+                    },
+                  )
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
