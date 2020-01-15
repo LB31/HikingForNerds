451c2e3e0c1593dcded6445cbdf03b2b7262f712
Jakub Müller <jakub@Jakubs-MacBook-Pro.local>
2020-01-15 15:50:14 +0100

    move map buttons to bottom

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -82,7 +82,7 @@ class MapPageState extends State<MapPage> {
 //           ),
           //TODO: remove mock button
           Align(
-            alignment: Alignment.bottomRight,
+            alignment: Alignment.centerRight,
             child: RawMaterialButton(
               onPressed: () {
                 showDialog(
