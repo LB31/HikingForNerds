421321a1280c615e0978baebb6415a4c71cb517b
Mario Schuetz <mayo2@gmx.de>
2020-01-23 20:11:31 +0100

    Remove unused import

--- lib/components/poi_category_search_bar.dart
+++ lib/components/poi_category_search_bar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:hiking4nerds/services/categories.dart';
 import 'package:hiking4nerds/services/localization_service.dart';
 import 'package:search_widget/search_widget.dart';
 
